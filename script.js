53b7d633a4c63dbdd44cf603b7937bc13ef39402
Abhishek Sharma <avisheksharma2004@gmail.com>
2024-05-01 04:02:46 +0530

    template Done

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+//For Color Selection
+const swatches = document.querySelectorAll('.color-swatch');
+        swatches.forEach(swatch => {
+            swatch.addEventListener('click', () => {
+                // Add your logic here for handling the click event
+                console.log(`Selected color: ${swatch.style.backgroundColor}`);
+            });
+        });
+ 
+
+
+        //For Product Quantity
+var numberElement = document.getElementById("number");
+var currentNumber = 1;
+
+  function increment() {
+    currentNumber++;
+    numberElement.textContent = currentNumber;
+  }
+
+  
+  function decrement() {
+    if (currentNumber > 1) {
+      currentNumber--;
+      numberElement.textContent = currentNumber;
+    }
+  }
+
+  
+  document.getElementById("plusBtn").addEventListener("click", function() {
+    increment();
+  });
+
+ 
+  document.getElementById("minusBtn").addEventListener("click", function() {
+    decrement();
+  });
+
+
+  //For add to cart button
+  function addToCart() {
+    alert("Item added to cart successfully!");
+}
+
+
+
+function changeMainImage(clickedImage) {
+    var mainImage = document.getElementById("mainImage");
+    mainImage.src = clickedImage.src;
+}
